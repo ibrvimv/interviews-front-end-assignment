6fe53d1a2903d9093137e33627ff813b5d8888d1
ibrvimv <ibrvimv@gmail.com>
2024-06-16 17:04:34 +0500

    setup

---
+++ frontend/lib/config.ts
@@ -0,0 +1,22 @@
+const config = {
+  siteName: 'RecipeBook',
+  siteDescription: 'RecipeBook - a community-driven recipe sharing platform',
+  author: 'Adlet Ibraimov',
+  authorUrl: 'https://www.instagram.com/ivluence',
+  nav: [
+    {
+      name: 'Main',
+      path: '/',
+    },
+    {
+      name: 'Cuisines',
+      path: '/cuisines',
+    },
+    {
+      name: 'Dietary',
+      path: '/dietary',
+    },
+  ],
+};
+
+export default config;
