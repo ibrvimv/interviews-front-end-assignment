0d673fa404e80f99ee3db10e65fab4230af8b0e9
ibrvimv <ibrvimv@gmail.com>
2024-06-19 13:41:15 +0500

    abstract

--- frontend/components/Home.tsx
+++ frontend/components/Home.tsx
@@ -5,10 +5,15 @@ import { useInView } from 'react-intersection-observer';
 import { RecipeItem } from '@/types/types';
 import RecipeCard from './RecipeCard';
 import Image from 'next/image';
+import FilterCategory from './FilterCategory';
+import Search from './Search';
+import LevelButton from './LevelButton';
 
 type HomeProps = {
   initialData: RecipeItem[];
 };
+
+const plans = ['Easy', 'Medium', 'Hard'];
 
 // used this component separate from main home page because:
 // I did not wanted to ruin SEO by making home page client component.
@@ -19,6 +24,7 @@ const Home: React.FC<HomeProps> = ({ initialData }) => {
   const [data, setData] = useState(initialData);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+
   const { ref, inView } = useInView({
     threshold: 1,
   });
@@ -46,59 +52,52 @@ const Home: React.FC<HomeProps> = ({ initialData }) => {
         <div className='max-w-lg w-full'>
           <h2 className='text-xl font-bold mb-10'>RecipeBook</h2>
           <form action='' className='flex flex-col gap-7'>
-            <div id='search' className='relative'>
+            <Search />
+            <div>
               <label className='block px-3 text-sm text-black  mb-3 relative max-w-md w-full '>
-                Search by Name
+                Select difficulty
               </label>
-              <input
-                className='max-w-md w-full py-4 px-3 rounded-xl text-black  focus:outline-none'
-                id='searchByName'
-                type='text'
-                placeholder='Enter recipe name'
-              />
-            </div>
-            <div id='filter'>
-              <div className='block px-3 text-sm text-black  mb-3  max-w-md w-full '>
-                Select category
+              <div className='flex gap-2 max-w-md  flex-1'>
+                <LevelButton level='easy' />
+                <LevelButton level='medium' />
+                <LevelButton level='hard' />
               </div>
-              <div className='flex gap-5'>
-                <div className='relative w-16 aspect-square '>
-                  <Image
-                    src='/spicy.svg'
-                    alt='spicy'
-                    fill
-                    className='object-cover'
-                  />
-                </div>
-                <div className='relative w-16 aspect-square '>
-                  <Image
-                    src='/vegan.svg'
-                    alt='vegan'
-                    fill
-                    className='object-cover'
-                  />
-                </div>
-                <div className='relative w-16 aspect-square '>
-                  <Image
-                    src='/gluten-free.svg'
-                    alt='gluten free'
-                    fill
-                    className='object-cover'
-                  />
-                </div>
-                <div className='relative w-16 aspect-square '>
-                  <Image
-                    src='/top.svg'
-                    alt='top rated'
-                    fill
-                    className='object-cover'
-                  />
-                </div>
+            </div>
+            <div>
+              <label className='block px-3 text-sm text-black  mb-3 relative max-w-md w-full '>
+                Select Diet
+              </label>
+              <div className='flex gap-2 max-w-md flex-wrap'>
+                <LevelButton level='Vegetarian' />
+                <LevelButton level='Mediterranean' />
+                <LevelButton level='Non-Vegetarian' />
+                <LevelButton level='Pescatarian' />
+              </div>
+            </div>
+            <div>
+              <label className='block px-3 text-sm text-black  mb-3 relative max-w-md w-full '>
+                Select Cuisines
+              </label>
+              <div className='flex gap-2 max-w-md flex-wrap'>
+                <LevelButton level='Italian' />
+                <LevelButton level='American' />
+                <LevelButton level='Mexican' />
+                <LevelButton level='Indian' />
+                <LevelButton level='Japanese' />
+                <LevelButton level='Spanish' />
+                <LevelButton level='French' />
+                <LevelButton level='Greek' />
+                <LevelButton level='Thai' />
+                <LevelButton level='British' />
+                <LevelButton level='Russian' />
+                <LevelButton level='Middle Eastern' />
+                <LevelButton level='North African' />
+                <LevelButton level='Korean' />
               </div>
             </div>
           </form>
         </div>
-        <div>
+        <div className='w-full'>
           <ul className='flex flex-col gap-5'>
             {data.map((item, index) => (
               <li key={index}>
