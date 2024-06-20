21bfc17131f1b23a39071cb0cb55b15b5786c024
ibrvimv <ibrvimv@gmail.com>
2024-06-20 17:30:47 +0500

    little fixes

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -8,9 +8,11 @@ interface PropsType {
 }
 export default function Button(props: PropsType) {
   return (
-    <button className={`bg-green rounded-2xl px-5 py-4 border-2 border-green  text-white hover:bg-back hover:text-green transition-all duration-50 ${props.text === '' ? '' : 'flex justify-between'} `}>
-      <div className={` ${props.text === '' ? '' : 'flex gap-2 items-center'} `}>
-        <div>{props?.text}</div>
+    <button className={`bg-green rounded-2xl px-5 h-14 border-2 border-green   text-white hover:bg-back hover:text-green transition-all duration-50 flex justify-between items-center`}>
+      <div className={`flex gap-2 items-center`}>
+        {
+          props.text && <div>{props?.text}</div>
+        }
         {
           props.icon && <props.icon />
         }
--- frontend/components/LevelButton.tsx
+++ frontend/components/LevelButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LevelButton = ({ level }: { level: string }) => {
   return (
-    <div className='flex justify-center items-center py-3 px-2 rounded-xl flex-auto bg-white text-black hover:text-green hover:shadow-2xl hover:shadow-green transition-all duration-50 cursor-pointer '>
+    <div className='flex justify-center items-center py-3 px-2 rounded-xl flex-auto text-xs font-bold  bg-white text-black hover:text-green hover:shadow-2xl hover:shadow-green transition-all duration-50 cursor-pointer '>
       {level}
     </div>
   );
