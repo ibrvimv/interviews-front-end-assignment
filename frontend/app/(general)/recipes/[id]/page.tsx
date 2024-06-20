bc2971a547040aab477bf93e0f833a2cadcf14c5
ibrvimv <ibrvimv@gmail.com>
2024-06-20 17:45:08 +0500

    comment

--- frontend/app/(general)/recipes/[id]/page.tsx
+++ frontend/app/(general)/recipes/[id]/page.tsx
@@ -1,6 +1,7 @@
-import { RecipeItem } from '@/types/types';
+import { Comments, RecipeItem } from '@/types/types';
 import Image from 'next/image';
 import { GetStaticPropsContext } from 'next';
+import { getComments } from '@/app/api/api';
 
 export async function generateStaticParams() {
   const posts: Array<RecipeItem> = await fetch('http://localhost:8080/recipes').then((res) => res.json())
@@ -23,33 +24,53 @@ const RecipePage = async ({ params }: { params: { id: string } }) => {
     return <div>Loading...</div>;
   }
 
+  const comments: Comments = await getComments()
+
   return (
-    <div className='flex gap-5'>
-      <div className='relative w-1/2 rounded-xl aspect-video overflow-hidden'>
-        <Image src={`http://localhost:8080${recipe?.image}`} alt={recipe.name} fill className='object-cover' />
-      </div>
-      <div className='w-1/2 aspect-video'>
-        <h1 className='font-bold text-xl mb-2'>{recipe.name}</h1>
-        <div className=' flex flex-col gap-2 justify-between h-full'>
-          <div>
-            <p className='mb-5'>{recipe.instructions}</p>
-            <div >
-              <h2 className='text-md font-semibold'>
-                Ingredients:
-              </h2>
-              {recipe.ingredients.map((ingredient, index) => {
-                return (
-                  <p className='text-xs font-thin' key={index}>
-                    {ingredient}
-                  </p>
-                );
-              })}
+    <div>
+      <div className='flex gap-5 mb-5'>
+        <div className='relative w-1/2 rounded-xl aspect-video overflow-hidden'>
+          <Image src={`http://localhost:8080${recipe?.image}`} alt={recipe.name} fill className='object-cover' />
+        </div>
+        <div className='w-1/2 aspect-video'>
+          <h1 className='font-bold text-xl mb-2'>{recipe.name}</h1>
+          <div className=' flex flex-col gap-2 justify-between h-full'>
+            <div>
+              <p className='mb-5'>{recipe.instructions}</p>
+              <div >
+                <h2 className='text-md font-semibold'>
+                  Ingredients:
+                </h2>
+                {recipe.ingredients.map((ingredient, index) => {
+                  return (
+                    <p className='text-xs font-thin' key={index}>
+                      {ingredient}
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
+            <div className='flex gap-5'>
+              <p className='text-md font-thin uppercase'>diet: {recipe.dietId}</p>
+              <p className='text-md font-thin uppercase'>difficulty: {recipe.difficultyId}</p>
             </div>
           </div>
-          <div className='flex gap-5'>
-            <p className='text-md font-thin uppercase'>diet: {recipe.dietId}</p>
-            <p className='text-md font-thin uppercase'>difficulty: {recipe.difficultyId}</p>
-          </div>
+        </div>
+      </div>
+      <div>
+        <div className='text-lg font-bold'>Reviews</div>
+        <div>
+          {
+            comments?.map((item, key) => {
+              return <div key={key}>
+                <div className='flex flex-col'>
+                  <div>{recipe.id === item.id ? item.comment : null}</div>
+                  <div> {recipe.id === item.id ? item.rating + ' Stars' : null}</div>
+                  <div>  {recipe.id === item.id ? item.date : null}</div>
+                </div>
+              </div>
+            })
+          }
         </div>
       </div>
     </div>
